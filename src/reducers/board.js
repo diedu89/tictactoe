704f7aa1178c8de47f444155a91cfc6da87a9787
Diego Meléndez <diedu89@gmail.com>
2017-11-16 01:35:26 -0600

    Added board reducer
    Test for board created
    Added deep-freeze package to check for immutability

---
+++ src/reducers/board.js
@@ -0,0 +1,17 @@
+const board = (state, action) => {
+	const { type, payload } = action;
+
+  switch (type) {
+    case 'MARK':
+      return state.map((item, index) => {
+      	if(index != action.row)
+      		return item;
+
+      	return item.map((item, index) => (index == action.col && item == null) ? action.player : item); 
+      });
+    default:
+      return state
+  }
+}
+
+export default board;
